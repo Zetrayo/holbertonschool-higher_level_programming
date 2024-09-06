285602427397c3391ee2db90da5872902ca19955
Zetrayo <josefeliciano.com98@gmail.com>
2024-09-06 16:54:18 +0200

    FIX : mistictacs

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 import sys
 
 if __name__ == "__main__":
-    argv = sys.argv[1:]
-    arg_count = len(argv)
-    if arg_count == 0:
-        print("{} argument.\n".format(arg_count), end="")
-    elif arg_count == 1:
-        print("{} argument:\n".format(arg_count), end="")
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments:\n".format(arg_count), end="")
-    for i, arg in enumerate(argv, 1):
-        print(f"{i}: {arg}")
+        print("{} arguments:".format(argc))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, sys.argv[i]))
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,4 +4,4 @@ import sys
 if __name__ == "__main__":
     argv = sys.argv[1:]
     total = sum(int(arg) for arg in argv)
-    print("{}\n".format(total), end="")
+    print(total)
