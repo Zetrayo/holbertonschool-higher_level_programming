cfe2ad87adab44a7b93af01d0caaef1eb0d20244
Zetrayo <josefeliciano.com98@gmail.com>
2024-09-19 14:43:32 +0200

    ADD : added file

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -3,21 +3,22 @@ class Square:
     """
     A class representing a square.
     """
-    '''setter'''
-    def size(self, value):
-        self.size = value
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
 
-    '''getter'''
+    @property
     def size(self):
-        return self.size
-
-    def __init__(self, size=0):
-        if not isinstance(size, int):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         return self.__size ** 2
